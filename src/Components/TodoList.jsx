f564466d97774a6cd225c1c82b59d331de2e0ee5
Morteza <morteza.abolfathi73@gmail.com>
2022-08-28 19:43:55 +0430

    update TodoList.jsx props Completed

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -4,9 +4,9 @@ import Todo from "./Todo";
 const TodoList = (props) => {
   return (
     <div>
-      {props.todoS.map((item) => 
-      <Todo todo={item} key={item.id}/>
-      )}
+      {props.todoS.map((item) => (
+        <Todo todo={item} key={item.id}  onCompleted={()=>props.onCompleted(item.id)}/>
+      ))}
     </div>
   );
 };
